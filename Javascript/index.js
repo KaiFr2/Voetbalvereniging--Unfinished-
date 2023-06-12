6530717d61a5bd34bf1964b850326d58da8f08a6
Joppe20003 <96177194+Joppe20003@users.noreply.github.com>
2023-06-12 10:00:27 +0200

    Add files via upload

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,11 @@
 function setDate(date) {
     window.location.href = window.location.pathname + "?date=" + date;
 }
+
+function showModal(title, location, description) {
+    document.getElementById('modalHeader').innerText = title;
+    document.getElementById('modalBodyOne').innerText = description;
+    document.getElementById('modalBodyTwo').innerText = "Locatie: " + location;
+
+    console.log(title, location, description);
+}
