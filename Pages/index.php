c3b0497af6f8ea4db61c68ef1c7ce3d55150cb42
Dogukan <564937@edu.rocmn.nl>
2023-06-13 10:00:12 +0200

    Alle files zijn terug en alles fixed inshallah

--- Pages/index.php
+++ Pages/index.php
@@ -15,13 +15,20 @@ include '../Particals/header.php';
 
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
-    <main role="main" class="inner cover">
-        <h1 class="text-white cover-heading">Welcome bij Voetbal Vereniging</h1>
-        <p class="text-white lead">Football is more than just a game; it's a passion that unites us, drives us, and defines us. Together, as a football club, we stand as a symbol of determination, teamwork, and unwavering spirit. Through the highs and lows, we forge our legacy on the field, inspiring generations and leaving an indelible mark on the beautiful game.</p>
-        <p class="lead">
-            <a href="#" class="text-white btn btn-lg btn-secondary" style="background-color: #D4AF37;">Learn more</a>
-        </p>
-    </main>
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 70vh;">
+            <div class="col-md-6">
+                <main role="main" class="inner cover text-center" style="margin-top: 30px;">
+                    <h1 class="text-white cover-heading" style="margin-bottom: 20px;">Welcome bij Voetbal Vereniging</h1>
+                    <p class="text-white lead" style="margin-bottom: 20px;">Football is more than just a game; it's a passion that unites us, drives us, and defines us. Together, as a football club, we stand as a symbol of determination, teamwork, and unwavering spirit. Through the highs and lows, we forge our legacy on the field, inspiring generations and leaving an indelible mark on the beautiful game.</p>
+                    <p class="lead">
+                        <a href="#" class="text-white btn btn-lg btn-secondary" style="background-color: #D4AF37;">Learn more</a>
+                    </p>
+                </main>
+            </div>
+        </div>
+    </div>
+
     <footer class="mastfoot mt-auto">
     </footer>
 </div>
