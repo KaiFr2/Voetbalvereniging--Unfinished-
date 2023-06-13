dd771ff0042c57b81dc04f988374f15e1a997cd2
Joppe20003 <96177194+Joppe20003@users.noreply.github.com>
2023-06-13 11:30:38 +0200

    Add files via upload

--- Pages/taken.php
+++ Pages/taken.php
@@ -1,3 +1,67 @@
 <?php
-echo 'test';
+include '../Particals/header.php';
 ?>
+<body class="bg-white">
+<div class="container-fluid pt-3">
+    <p class="m-1">Uitleg</p>
+    <h3 class=" mt-3 mb-3 m-1 p-1 border-bottom border-custom-gold border-1">Beschikbare taken</h3>
+    <div class="bg-custom-1 row m-1 p-1 pt-2 pb-2">
+        <div class="col-lg-3">
+            <div class="bg-white border-start border-2 border-custom-gold rounded">
+                <h6 class="m-1">test</h6>
+                <p class="m-1 mt-2">teststststts</p>
+                <p class="m-1 mt-2">Datum: 09-08-2023</p>
+                <div class="d-flex" style="justify-content: space-between !important;">
+                    <p class="m-1">Tijd: 11:50 - 12:50</p>
+                    <p class="m-1">0/6</p>
+                </div>
+                <div style="display: flex !important; justify-content: center !important;">
+                    <div class="w-100 m-1 mt-2 btn btn-success">test</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="bg-white border-start border-2 border-custom-gold rounded">
+                <h6 class="m-1">test</h6>
+                <p class="m-1 mt-2">teststststts</p>
+                <p class="m-1 mt-2">Datum: 09-08-2023</p>
+                <div class="d-flex" style="justify-content: space-between !important;">
+                    <p class="m-1">Tijd: 11:50 - 12:50</p>
+                    <p class="m-1">0/6</p>
+                </div>
+                <div style="display: flex !important; justify-content: center !important;">
+                    <div class="w-100 m-1 mt-2 btn btn-success">test</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="bg-white border-start border-2 border-custom-gold rounded">
+                <h6 class="m-1">test</h6>
+                <p class="m-1 mt-2">teststststts</p>
+                <p class="m-1 mt-2">Datum: 09-08-2023</p>
+                <div class="d-flex" style="justify-content: space-between !important;">
+                    <p class="m-1">Tijd: 11:50 - 12:50</p>
+                    <p class="m-1">0/6</p>
+                </div>
+                <div style="display: flex !important; justify-content: center !important;">
+                    <div class="w-100 m-1 mt-2 btn btn-success">test</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="bg-white border-start border-2 border-custom-gold rounded">
+                <h6 class="m-1">test</h6>
+                <p class="m-1 mt-2">teststststts</p>
+                <p class="m-1 mt-2">Datum: 09-08-2023</p>
+                <div class="d-flex" style="justify-content: space-between !important;">
+                    <p class="m-1">Tijd: 11:50 - 12:50</p>
+                    <p class="m-1">0/6</p>
+                </div>
+                <div style="display: flex !important; justify-content: center !important;">
+                    <div class="w-100 m-1 mt-2 btn btn-success">test</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
